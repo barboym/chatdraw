b7d9fd95699e6a0c31225a4e69cc10944b726a72
moshe <barboym@yahoo.com>
2025-07-08 10:27:26 +0000

    added debugpy to devcontainer

--- scripts/create_rerun_db.sh
+++ scripts/create_rerun_db.sh
@@ -1,7 +1,26 @@
 set -a; source ../.env; set +a
-docker run -d \
-  --name my-postgres \
-  -e POSTGRES_PASSWORD=$DB_PASSWORD \
-  -v ${DB_LOCALPATH}:/var/lib/postgresql/data \
-  -p 5432:${DB_PORT} \
-  postgres:17
+
+CONTAINER_NAME="my-postgres"
+NETWORK_NAME="my-network"
+
+# Check if the network exists
+if ! docker network inspect "$NETWORK_NAME" >/dev/null 2>&1; then
+  echo "Creating Docker network: $NETWORK_NAME"
+  docker network create "$NETWORK_NAME"
+else
+  echo "Docker network '$NETWORK_NAME' already exists."
+fi
+docker network create my-network
+
+if [ "$(docker ps -a -q -f name=^/${CONTAINER_NAME}$)" ]; then
+  docker start "$CONTAINER_NAME"
+else
+  echo "Container '$CONTAINER_NAME' does not exist."
+  docker run -d \
+    --name $CONTAINER_NAME \
+    -e POSTGRES_PASSWORD=$DB_PASSWORD \
+    -v ${DB_LOCALPATH}:/var/lib/postgresql/data \
+    -p 5432:${DB_PORT} \
+    --network my-network \
+    postgres:17
+fi
--- tests/test_db_connection.py
+++ tests/test_db_connection.py
@@ -8,7 +8,8 @@ dotenv.load_dotenv()
 
 
 if __name__=="__main__":
-    conn = psycopg2.connect("dbname=sketchs user=myuser")
+    conn = psycopg2.connect("dbname=sketches user=myuser")
     cur = conn.cursor()
-    cur.execute("SELECT sketch_data FROM sketches WHERE concept = %s", (concept,))
+    cur.execute("SELECT sketch_id FROM sketches WHERE concept = giraffe")
     result = cur.fetchone()
+    print(result)
